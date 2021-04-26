ff4cfaf592b114dd06e1de354c479c272fcd72fa
Владислав <vlados-pro200@mail.ru>
2021-04-26 20:49:12 +0400

    Release v1

---
+++ AddEditPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Trainees _currentTraine = new Trainees();
+
+        public AddEditPage(Trainees selectedTrainees)
+        {
+            InitializeComponent();
+
+            if (selectedTrainees != null)
+                _currentTraine = selectedTrainees;
+
+            DataContext = _currentTraine;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentTraine.FirstName))
+                errors.AppendLine("Укажите имя");
+
+            if (string.IsNullOrWhiteSpace(_currentTraine.Surname))
+                errors.AppendLine("Укажите фамилию");
+
+            if (string.IsNullOrWhiteSpace(_currentTraine.Patronymic))
+                errors.AppendLine("Укажите отчество");
+
+            if (_currentTraine.Age < 18 || _currentTraine.Age > 80)
+                errors.AppendLine("Укажите возраст от 18 до 80");
+
+            if (string.IsNullOrWhiteSpace(_currentTraine.Classification))
+                errors.AppendLine("Укажите уровень");
+
+            if (string.IsNullOrWhiteSpace(_currentTraine.TheDepartment))
+                errors.AppendLine("Укажите отдел");
+
+            if (_currentTraine.Salary < 0 || _currentTraine.Salary > 100000)
+                errors.AppendLine("Укажите заплату от 0 до 100000");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentTraine.id == 0)
+                CompanyDatabaseEntities.GetContext().Trainees.Add(_currentTraine);
+
+
+            try
+            {
+                CompanyDatabaseEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack(); 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            
+        }
+    }
+}
---
+++ Base.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Base.xaml
+    /// </summary>
+    public partial class Base : Page
+    {
+        public Base()
+        {
+            InitializeComponent();
+            //DGridBase.ItemsSource = CompanyDatabaseEntities.GetContext().Trainees.ToList();
+        }
+
+       
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Trainees));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var remove = DGridBase.SelectedItems.Cast<Trainees>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {remove.Count()} элементов?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    CompanyDatabaseEntities.GetContext().Trainees.RemoveRange(remove);
+                    CompanyDatabaseEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+
+                    DGridBase.ItemsSource = CompanyDatabaseEntities.GetContext().Trainees.ToList();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+                if (Visibility == Visibility.Visible)
+            {
+                CompanyDatabaseEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridBase.ItemsSource = CompanyDatabaseEntities.GetContext().Trainees.ToList();
+            }
+        }
+    }
+}
